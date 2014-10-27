05041b5c27b24728e296352ee4354cf9b06563c5
FoKycHuK <fokychuk47@ya.ru>
2014-10-27 22:40:29 +0600

    останавливается земля.

--- Assets/creater.cs
+++ Assets/creater.cs
@@ -14,15 +14,9 @@ public partial class creater : MonoBehaviour
     IWorld world;
     GUIText scoresTextLeft;
     GUIText scoresTextRight;
-    //public GUIText scoresTextLeftPref;
-    //public GUIText scoresTextRightPref;
     GameObject myCamera;
     public GameObject cubePref; // Эти поля -- прототипы, к ним самим обращаться не получится.
-    //public GameObject planePref; // Для этого, нужно найти объект в мире каким-либо образом.
-    //public GameObject cameraPref; // Например: GameObject.Find(name); написал, чтоб не забыть.
-    // А вот так можно получить все объекты в мире, а потом уже выбирать:
-    // var allGameObjects = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
-
+    bool worldRunning = true;
 
     // Use this for initialization
 
@@ -33,46 +27,49 @@ public partial class creater : MonoBehaviour
         Behaviour = this;
         CameraCreator();
         ScoresFieldsCreator();
-        //Instantiate(cameraPref, new Vector3(0, 20, 0), Quaternion.Euler(90, 0, 0));
-        //Instantiate(planePref, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
-
         world = CreateDemoWorld();
-        //world = CreateDemoWorld();
         world.Scores.ScoresChanged += UpdateScores;
         CollisionInfo = new Tuple<string, string, int>(null, null, 0);
-        //scoresTextLeft = Instantiate(scoresTextLeftPref) as GUIText;
-        //scoresTextRight = Instantiate(scoresTextRightPref) as GUIText;
-        //scoresTextLeft.text = "Left Scores: 0";
-        //scoresTextRight.text = "Right Scores: 0";
-        //world.Scores.ScoresChanged += UpdateScores;
+
     }
 
     void Update()
     {
-        if (CollisionInfo.Item3 == 2)
-        {
-            ((UEngine)world.Engine).CollisionSender(CollisionInfo.Item1, CollisionInfo.Item2);
-            CollisionInfo.Item3 = 0;
-        }
-        //UKeyboard a = new UKeyboard();
-        //foreach (string i in a.PressedKeys)
-        //    Debug.Log(i);
+        //Debug.Log(Time.timeScale);
+        //timer++;
+        //if (timer == 100)
+        //{
+        //    Time.timeScale = 10;
+        //}
+        //if (timer == 200)
+        //{
+        //    Time.timeScale = 0.5f;
+        //}
+        //ТОЛЬКО В АПДЕЙТЕ (НЕ В ФИКСЕД_АПДЕЙТЕ)
+        //напомнить, > чтобы я глянул, что передается в таймер
 
-    }
 
-    bool worldRunning = true;
-
-    void FixedUpdate()
-    {
         if (!worldRunning) return;
         if (Time.fixedTime > world.Clocks.TimeLimit)
         {
             worldRunning = false;
             world.OnExit();
-            ((UEngine)world.Engine).Stop();
-            ((UEngine)world.Engine).UpdateSpeeds();
+            Time.timeScale = 0;
+            //((UEngine)world.Engine).Stop();
+            //((UEngine)world.Engine).UpdateSpeeds();
             return;
         }
+
+
+        if (CollisionInfo.Item3 == 2)
+        {
+            ((UEngine)world.Engine).CollisionSender(CollisionInfo.Item1, CollisionInfo.Item2);
+            CollisionInfo.Item3 = 0;
+        }
+    }
+
+    void FixedUpdate() //только физика и строгие расчеты. вызывается строго каждые 20 мс
+    {
         world.Clocks.Tick(Time.fixedTime);
         ((UEngine)world.Engine).UpdateSpeeds();
     }
